66a86a8d21d17149d9f2e3dcc3a7297d9782f37c
mvind <vms754@protonmail.com>
2018-08-12 20:54:15 +0200

    Completed tkinter skeleton.

---
+++ main.py
@@ -0,0 +1,25 @@
+from tkinter import *
+from tkinter import filedialog
+import os
+
+def main():
+
+
+    uploadButton = Button(root, text="Select file", command=load_file)
+    uploadButton.pack()
+
+    mainloop()
+
+def load_file():
+    dir_path = os.getcwd()
+    try:
+        root.filename = filedialog.askopenfile(initialdir = dir_path ,title='Select file'
+                        , filetypes=(("PDF files",".PDF"),('All files','*.*')))
+    except:
+        print('error')
+
+    print(root.filename)
+
+if __name__ == '__main__':
+    root = Tk()
+    main()
